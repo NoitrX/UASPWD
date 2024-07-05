852c0569a57ea3c62d3b9fbcc6465c95946f2e75
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-07-05 10:13:28 +0700

    Fixing Bugs JS

---
+++ assets/js/cart.js
@@ -0,0 +1,82 @@
+function addToCart(e) {
+  const button = e.target;
+  const product = {
+    id: button.getAttribute("data-id"),
+    name: button.getAttribute("data-name"),
+    price: parseFloat(button.getAttribute("data-price")),
+    qty: 1,
+  };
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const productIndex = cart.findIndex((item) => item.id === product.id);
+  if (productIndex !== -1) {
+    alert("Produk inisudah ada di keranjang");
+  } else {
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Produk Berhasil Ditambahkan");
+    updateCartTable();
+  }
+}
+
+function updateCartTable() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cartItemsContainer = document.getElementById("cart-items");
+  cartItemsContainer.innerHTML = "";
+
+  cart.forEach((item) => {
+    const row = document.createElement("tr");
+    row.classList.add("py-6");
+    row.innerHTML = `
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500 dark:text-neutral-200">${item.name}</td>
+        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-200">${item.price}</td>
+        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-200">${item.qty}</td>
+        <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">${item.price * item.qty}</td>
+        <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+          <button class="mx-2" onclick="updateQuantity('${item.id}', -1)">-</button>
+          <button onclick="updateQuantity('${item.id}', 1)">+</button>
+        </td>
+      `;
+    cartItemsContainer.appendChild(row);
+  });
+
+  const grandTotal = calculateGrandTotal(cart);
+  const grandTotalElement = document.getElementById("grand-total");
+  grandTotalElement.textContent = `GRANDTOTAL: Rp. ${grandTotal}`;
+}
+
+function updateQuantity(id, change) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const productIndex = cart.findIndex((item) => item.id === id);
+  if (productIndex !== -1) {
+    cart[productIndex].qty += change;
+    if (cart[productIndex].qty <= 0) {
+      cart.splice(productIndex, 1);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartTable();
+  }
+}
+
+const checkOut = () => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let checkoutHistory = JSON.parse(localStorage.getItem("checkoutHistory")) || [];
+  const grandTotal = calculateGrandTotal(cart);
+  checkoutHistory.push({ items: cart, grandTotal: grandTotal, checkoutTime: new Date() });
+
+  localStorage.removeItem("cart");
+
+  localStorage.setItem("checkoutHistory", JSON.stringify(checkoutHistory));
+
+  alert("Checkout successful");
+  updateCartTable();
+};
+
+function calculateGrandTotal(cart) {
+  return cart.reduce((total, item) => total + item.price * item.qty, 0);
+}
+document.querySelectorAll("button[data-id]").forEach((button) => {
+  button.addEventListener("click", addToCart);
+});
+
+updateCartTable();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,21 @@
-function addToCart(e) {
-  const button = e.target;
-  const product = {
-    id: button.getAttribute("data-id"),
-    name: button.getAttribute("data-name"),
-    price: parseFloat(button.getAttribute("data-price")),
-    qty: 1,
-  };
+function getPesananTable() {
+  let checkOutHistory = JSON.parse(localStorage.getItem("checkoutHistory")) || [];
+  const pesananTable = document.getElementById("table-pesanan");
+  pesananTable.innerHTML = "";
 
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const productIndex = cart.findIndex((item) => item.id === product.id);
-  if (productIndex !== -1) {
-    alert("Product is already in the cart");
-  } else {
-    cart.push(product);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    updateCartTable();
-  }
-}
-
-function updateCartTable() {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const cartItemsContainer = document.getElementById("cart-items");
-  cartItemsContainer.innerHTML = "";
-
-  cart.forEach((item) => {
-    const row = document.createElement("tr");
-    row.classList.add("py-6");
-    row.innerHTML = `
-      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500 dark:text-neutral-200">${item.name}</td>
-      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-200">${item.price}</td>
-      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-neutral-200">${item.qty}</td>
-      <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">${item.price * item.qty}</td>
-      <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-        <button class="mx-2" onclick="updateQuantity('${item.id}', -1)">-</button>
-        <button onclick="updateQuantity('${item.id}', 1)">+</button>
-      </td>
-    `;
-    cartItemsContainer.appendChild(row);
+  checkOutHistory.forEach((item) => {
+    item.items.forEach((product, index) => {
+      const row = document.createElement("tr");
+      row.classList.add("py-6");
+      row.innerHTML = `
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500 dark:text-neutral-200">${product.name} (${product.qty})</td>
+        ${index === 0 ? `<td rowspan="${item.items.length}" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500 dark:text-neutral-200">${new Date(item.checkoutTime)}</td>` : ""}
+        ${index === 0 ? `<td rowspan="${item.items.length}" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500 dark:text-neutral-200">Rp. ${item.grandTotal}</td>` : ""}
+       
+        `;
+      pesananTable.appendChild(row);
+    });
   });
-
-  const grandTotal = calculateGrandTotal(cart);
-  const grandTotalElement = document.getElementById("grand-total");
-  grandTotalElement.textContent = `GRANDTOTAL: Rp. ${grandTotal}`;
 }
 
-function updateQuantity(id, change) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const productIndex = cart.findIndex((item) => item.id === id);
-  if (productIndex !== -1) {
-    cart[productIndex].qty += change;
-    if (cart[productIndex].qty <= 0) {
-      cart.splice(productIndex, 1);
-    }
-    localStorage.setItem("cart", JSON.stringify(cart));
-    updateCartTable();
-  }
-}
-
-function calculateGrandTotal(cart) {
-  return cart.reduce((total, item) => total + item.price * item.qty, 0);
-}
-
-const checkOut = () => {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  let checkoutHistory = JSON.parse(localStorage.getItem("checkoutHistory")) || [];
-
-  checkoutHistory.push({ items: cart, checkoutTime: new Date() });
-
-  localStorage.removeItem("cart");
-
-  localStorage.setItem("checkoutHistory", JSON.stringify(checkoutHistory));
-
-  console.log("Checkout successful");
-  updateCartTable();
-};
-updateCartTable();
-
-document.querySelectorAll("button[data-id]").forEach((button) => {
-  button.addEventListener("click", addToCart);
-});
+getPesananTable();
